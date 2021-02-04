b38dcd44bfed4e25e45df414c504e7912e7d3b18
otacrazy2123 <andre.sousa.tec@gmail.com>
2021-02-04 10:39:44 -0300

    Initial commit

---
+++ src/main/java/br/com/desafiozup/tentativatres/TentativatresApplication.java
@@ -0,0 +1,13 @@
+package br.com.desafiozup.tentativatres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TentativatresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TentativatresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/desafiozup/tentativatres/TentativatresApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.desafiozup.tentativatres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TentativatresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
