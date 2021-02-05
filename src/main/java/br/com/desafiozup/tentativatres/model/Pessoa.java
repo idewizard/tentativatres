e1fbdc0e9d87ada3e11d6355ae88f20fde3e0128
otacrazy2123 <andre.sousa.tec@gmail.com>
2021-02-05 13:41:17 -0300

    model pessoa

--- src/main/java/br/com/desafiozup/tentativatres/model/Pessoa.java
+++ src/main/java/br/com/desafiozup/tentativatres/model/Pessoa.java
@@ -1,5 +1,32 @@
 package br.com.desafiozup.tentativatres.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table( name = "pessoa")
 public class Pessoa {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "idpessoa")
+	private long id;
+	
+	@Column(name = "nome" , nullable = false)
+	private String nome;
+	
+	@Column(name = "sobrenome")
+	private String sobrenome;
+	
+	@Column(name = "email" , nullable = false , unique = true)
+	private String email;
+	
+	
 }
